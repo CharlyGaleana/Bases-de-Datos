5680c22bf50824e54ad558ebd300334f0a6eb5d6
JacquelineLira <31213215+JacquelineLira@users.noreply.github.com>
2018-04-05 20:17:19 -0500

    Tarea 6 Version 2

--- Tarea6/Tarea6Web/AltaPedido.aspx
+++ Tarea6/Tarea6Web/AltaPedido.aspx
@@ -8,32 +8,51 @@
     <title></title>
 </head>
 <body style="height: 254px">
-    <form id="form1" runat="server" style="background-color: #33CCFF; height: 363px;">
+    <form id="form1" runat="server" style="background-color: #33CCFF; height: 908px;">
     <div style="font-size: 20px">
     
         <asp:Label ID="Label1" runat="server" style="z-index: 1; left: 504px; top: 17px; position: absolute; height: 67px; width: 132px" Text="Alta de Pedidos"></asp:Label>
     
     </div>
-        <asp:Label ID="Label2" runat="server" Text="Agregar Artículos"></asp:Label>
+        <asp:Label ID="Label2" runat="server" Text="Agregar Artículos" style="z-index: 1; left: 82px; top: 35px; position: absolute"></asp:Label>
         <br />
+        <asp:Label ID="Label6" runat="server" style="z-index: 1; left: 381px; top: 70px; position: absolute; right: 813px" Text="Clientes: " Visible="False"></asp:Label>
         <br />
-        <asp:Label ID="Label3" runat="server" Text="Artículo:  "></asp:Label>
-        <asp:DropDownList ID="Articulos" runat="server" style="z-index: 1; left: 74px; top: 72px; position: absolute">
+        <asp:Label ID="Label3" runat="server" Text="Artículo:  " style="z-index: 1; left: 12px; top: 73px; position: absolute"></asp:Label>
+        <asp:DropDownList ID="Articulos" runat="server" style="z-index: 1; left: 92px; top: 72px; position: absolute">
         </asp:DropDownList>
-        <p>
-            <asp:Label ID="Label4" runat="server" Text="Cantidad:  "></asp:Label>
-            <asp:TextBox ID="Cantidad" runat="server" style="z-index: 1; left: 72px; top: 113px; position: absolute; width: 83px"></asp:TextBox>
-        </p>
-        <asp:Button ID="Articulo" runat="server" Text="Agregar Articulo" OnClick="Articulo_Click" />
-        <asp:GridView ID="Detalles" runat="server" AutoGenerateColumns="False">
-            <Columns>
-                <asp:BoundField HeaderText="Id" />
-                <asp:BoundField HeaderText="Nombre" />
-                <asp:BoundField HeaderText="Precio" />
-                <asp:BoundField HeaderText="Cantidad" />
-                <asp:BoundField HeaderText="Monto Total" />
-            </Columns>
+            <asp:TextBox ID="Cantidad" runat="server" style="z-index: 1; left: 90px; top: 112px; position: absolute; width: 83px; right: 1064px;"></asp:TextBox>
+        <asp:Button ID="Articulo" runat="server" Text="Agregar Articulo" OnClick="Articulo_Click" style="z-index: 1; left: 10px; top: 151px; position: absolute" />
+        <asp:Label ID="Label4" runat="server" style="z-index: 1; left: 14px; top: 117px; position: absolute" Text="Cantidad"></asp:Label>
+        <asp:GridView ID="GridViewArticulos" runat="server" style="z-index: 1; left: 15px; top: 226px; position: absolute; height: 133px; width: 187px">
         </asp:GridView>
+        <asp:Table ID="tblDetalles" runat="server" style="z-index: 1; left: 19px; top: 498px; position: absolute; height: 25px; width: 306px" Visible="False">
+            <asp:TableRow runat="server">
+                <asp:TableCell runat="server">FolioP</asp:TableCell>
+                <asp:TableCell runat="server">FechaPed</asp:TableCell>
+                <asp:TableCell runat="server">FechaFin</asp:TableCell>
+                <asp:TableCell runat="server">Monto</asp:TableCell>
+                <asp:TableCell runat="server">SaldoCli</asp:TableCell>
+                <asp:TableCell runat="server">SaldoFacs</asp:TableCell>
+                <asp:TableCell runat="server">RFCC</asp:TableCell>
+                <asp:TableCell runat="server">RFCE</asp:TableCell>
+            </asp:TableRow>
+            <asp:TableRow runat="server">
+                <asp:TableCell runat="server"></asp:TableCell>
+                <asp:TableCell runat="server"></asp:TableCell>
+                <asp:TableCell runat="server"></asp:TableCell>
+                <asp:TableCell runat="server"></asp:TableCell>
+                <asp:TableCell runat="server"></asp:TableCell>
+                <asp:TableCell runat="server"></asp:TableCell>
+                <asp:TableCell runat="server"></asp:TableCell>
+                <asp:TableCell runat="server"></asp:TableCell>
+            </asp:TableRow>
+        </asp:Table>
+        <asp:Button ID="Generar" runat="server" OnClick="Generar_Click" style="z-index: 1; left: 11px; top: 184px; position: absolute; width: 139px" Text="Generar Pedido" />
+        <asp:Label ID="Label5" runat="server" style="z-index: 1; left: 389px; top: 332px; position: absolute" Text="Total: "></asp:Label>
+        <asp:Label ID="Total" runat="server" style="z-index: 1; left: 438px; top: 332px; position: absolute"></asp:Label>
+        <asp:DropDownList ID="Clientes" runat="server" style="z-index: 1; left: 448px; top: 69px; position: absolute" Visible="False">
+        </asp:DropDownList>
     </form>
 </body>
 </html>
--- Tarea6/Tarea6Web/AltaPedido.aspx.cs
+++ Tarea6/Tarea6Web/AltaPedido.aspx.cs
@@ -8,37 +8,153 @@ using System.Data;
 
 public partial class AltaPedido : System.Web.UI.Page
 {
-    //Variables de la clase 
+    //Variables de la clase
+    //Variables de la dataTable 
     DataTable dt;
+    DataRow dr;
+    DataColumn dc;
+    DataView dv;
+    //Gestor y Comunes
     GestorBD.GestorBD GestorBD;
     Comunes objeto = new Comunes();
    //Data Sets
     DataSet dsArticulo=new DataSet();
+    DataSet dsGeneral = new DataSet();
+    DataSet dsUsuarios = new DataSet();
     String cadSql;
+    private void crearTable()
+    {
+        dt = new DataTable();
+        //Crear las columnas
+        //IdArtículo
+        dc = new DataColumn();
+        dc.DataType = System.Type.GetType("System.Int32");
+        dc.ColumnName = "Id";
+        dt.Columns.Add(dc);
+        //Nombre
+        dc = new DataColumn();
+        dc.DataType = System.Type.GetType("System.String");
+        dc.ColumnName = "Nombre";
+        dt.Columns.Add(dc);
+        //Precio
+        dc = new DataColumn();
+        dc.DataType = System.Type.GetType("System.Int32");
+        dc.ColumnName = "Precio";
+        dt.Columns.Add(dc);
+        //Cantidad
+        dc = new DataColumn();
+        dc.DataType = System.Type.GetType("System.Int32");
+        dc.ColumnName = "Cantidad";
+        dt.Columns.Add(dc);
+        //Monto Total
+        dc = new DataColumn();
+        dc.DataType = System.Type.GetType("System.Int32");
+        dc.ColumnName = "Monto Total";
+        dt.Columns.Add(dc);
+        Session["dataTable"] = dt;
+        GridViewArticulos.DataSource = dv;
+        GridViewArticulos.DataBind();
+    }
     protected void Page_Load(object sender, EventArgs e)
     {
         if (!IsPostBack)
         {
+            Total.Text = "0.00";
             //Recuperar el gestor
             GestorBD = (GestorBD.GestorBD)Session["GestorBD"];
             //LLenar la ddl de Artículos
             cadSql = "select * from PCArtículos";
+
             GestorBD.consBD(cadSql, dsArticulo, "Artículo");
             objeto.cargaDDL(Articulos, dsArticulo, "Artículo", "Nombre");
             Session["DSArticulo"] = dsArticulo;
-            //
-            dt = new DataTable();
-            dt.Columns.Add(new DataColumn("Id", typeof(Int32)));
-            dt.Columns.Add(new DataColumn("Nombre", typeof(string)));
-            dt.Columns.Add(new DataColumn("Precio", typeof(Int32)));
-            dt.Columns.Add(new DataColumn("Cantidad", typeof(Int16)));
-            dt.Columns.Add(new DataColumn("Monto Total", typeof(Int32)));
+            crearTable();
+            tblDetalles.Visible = false;
+            //Si no es cliente que le de una lista de clientes para encargar
+            if (Session["Tipo"].ToString() != "Cli")
+            {
+                Clientes.Visible = true;
+                Label6.Visible = true;
+                cadSql = "select * from PCUsuarios";
+                GestorBD.consBD(cadSql, dsUsuarios, "Usuarios");
+                objeto.cargaDDL(Clientes, dsUsuarios, "Usuarios", "RFC");
+            }
         }
     }
 
     protected void Articulo_Click(object sender, EventArgs e)
     {
-        int id; String Nombre; int Precio; int cantidad; int montoTotal;
-        DataRow dr = dt.NewRow();
+        Double total = Double.Parse(Total.Text);
+        tblDetalles.Visible = false;
+        //Agregar la información
+        dt = (DataTable)Session["dataTable"];
+        int id; String Nombre = Articulos.SelectedValue; ; Double Precio;
+        int cantidad = Int32.Parse(Cantidad.Text); Double montoTotal;
+        cadSql = "select * from PCArtículos where Nombre='" + Nombre + "'";
+        GestorBD = (GestorBD.GestorBD)Session["GestorBD"];
+        GestorBD.consBD(cadSql, dsArticulo, "Info");
+        dr = dsArticulo.Tables["Info"].Rows[0];
+        id = Int32.Parse(dr["IdArt"].ToString());
+        Precio = Double.Parse(dr["Precio"].ToString());
+        montoTotal = cantidad * Precio;
+        total = total + montoTotal;
+        Total.Text = total+"";
+
+        dr = dt.NewRow();
+        dr["Id"] = id;
+        dr["Nombre"] = Nombre;
+        dr["Precio"] = Precio;
+        dr["Cantidad"] = cantidad;
+        dr["Monto Total"] = montoTotal;
+        dt.Rows.Add(dr);
+        dv = new DataView(dt);
+        GridViewArticulos.DataSource = dv;
+        GridViewArticulos.DataBind();
+        Session["dataTable"] = dt;
+    }
+
+    protected void Generar_Click(object sender, EventArgs e)
+    {
+        GestorBD = (GestorBD.GestorBD)Session["GestorBD"];
+        dt = (DataTable)Session["dataTable"];
+        //Generas el pedido
+        cadSql = "select max(folioP) as MaxFolio from PcPedidos";
+        GestorBD.consBD(cadSql, dsGeneral, "MaxF");
+        dr = dsGeneral.Tables["MaxF"].Rows[0];
+        int folioP = Int32.Parse(dr["MaxFolio"].ToString())+1;
+        String fechaPed = "'" + DateTime.Now.Year.ToString() +"-"+ DateTime.Now.Month.ToString() +"-"+ DateTime.Now.Day.ToString() + "'";
+        String fechaFin = null;
+        Double Monto = Double.Parse(Total.Text);
+        //Saldo Cli y saldo facturas es 0
+        String RFCE,RFCC;
+        if (Session["Tipo"].ToString() == "Cli")
+        {
+            RFCE = null;
+            RFCC = Session["RFC"].ToString();
+        }
+        else
+        {
+            RFCE = Session["rfc"].ToString();
+            RFCC = Clientes.SelectedValue;
+        }
+        cadSql = "insert into PCPedidos values("+folioP+",date"+fechaPed+",null,"+Monto+",0.00,0.00,'"+RFCC+"','"+RFCE+"')";
+        GestorBD.altaBD(cadSql);
+        tblDetalles.Rows[0].Cells[0].Text = folioP.ToString();
+        tblDetalles.Rows[0].Cells[1].Text = fechaPed;
+        tblDetalles.Rows[0].Cells[2].Text = "---";
+        tblDetalles.Rows[0].Cells[3].Text = Monto.ToString();
+        tblDetalles.Rows[0].Cells[4].Text = "0.00";
+        tblDetalles.Rows[0].Cells[5].Text = "0.00";
+        tblDetalles.Rows[0].Cells[6].Text = RFCC;
+        tblDetalles.Rows[0].Cells[7].Text = RFCE;
+        foreach (DataRow row in dt.Rows)
+        {
+            int idArt=Int32.Parse(row["id"].ToString());
+            int cantPed = Int32.Parse(row["cantidad"].ToString());
+            cadSql = "insert into PCDetalle values(" + folioP + "," + idArt + "," + cantPed + ",0)";
+            GestorBD.altaBD(cadSql);
+        }
+        crearTable();
+        tblDetalles.Visible = true;
     }
 }
